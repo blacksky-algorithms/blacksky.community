8b2139780da5af701700e3acfc9e44cd5cc02bcc
Samuel Newman <mozzius@protonmail.com>
2025-07-02 00:36:04 +0300

    Activity notification settings (#8485)
    Co-authored-by: Eric Bailey <git@esb.lol>
    Co-authored-by: Samuel Newman <mozzius@protonmail.com>
    Co-authored-by: hailey <me@haileyok.com>

--- src/state/queries/nuxs/definitions.ts
+++ src/state/queries/nuxs/definitions.ts
@@ -7,13 +7,6 @@ export enum Nux {
   ExploreInterestsCard = 'ExploreInterestsCard',
   InitialVerificationAnnouncement = 'InitialVerificationAnnouncement',
   ActivitySubscriptions = 'ActivitySubscriptions',
-  AgeAssuranceDismissibleNotice = 'AgeAssuranceDismissibleNotice',
-  AgeAssuranceDismissibleFeedBanner = 'AgeAssuranceDismissibleFeedBanner',
-
-  /*
-   * Blocking announcements. New IDs are required for each new announcement.
-   */
-  PolicyUpdate202508 = 'PolicyUpdate202508',
 }
 
 export const nuxNames = new Set(Object.values(Nux))
@@ -35,18 +28,6 @@ export type AppNux = BaseNux<
       id: Nux.ActivitySubscriptions
       data: undefined
     }
-  | {
-      id: Nux.AgeAssuranceDismissibleNotice
-      data: undefined
-    }
-  | {
-      id: Nux.AgeAssuranceDismissibleFeedBanner
-      data: undefined
-    }
-  | {
-      id: Nux.PolicyUpdate202508
-      data: undefined
-    }
 >
 
 export const NuxSchemas: Record<Nux, zod.ZodObject<any> | undefined> = {
@@ -54,7 +35,4 @@ export const NuxSchemas: Record<Nux, zod.ZodObject<any> | undefined> = {
   [Nux.ExploreInterestsCard]: undefined,
   [Nux.InitialVerificationAnnouncement]: undefined,
   [Nux.ActivitySubscriptions]: undefined,
-  [Nux.AgeAssuranceDismissibleNotice]: undefined,
-  [Nux.AgeAssuranceDismissibleFeedBanner]: undefined,
-  [Nux.PolicyUpdate202508]: undefined,
 }
