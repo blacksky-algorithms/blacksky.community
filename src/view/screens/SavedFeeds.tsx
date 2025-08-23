0352010c0170c9fbf4a03ff16d42b9aa7a1f9aba
Rudy Fraser <him@rudyfraser.com>
2025-08-23 15:32:16 -0400

    Update links and branding

--- src/view/screens/SavedFeeds.tsx
+++ src/view/screens/SavedFeeds.tsx
@@ -208,7 +208,7 @@ function SavedFeedsInner({
               <TextLink
                 type="sm"
                 style={pal.link}
-                href="https://github.com/bluesky-social/feed-generator"
+                href="https://github.com/blacksky-algorithms/rsky/tree/main/rsky-feedgen"
                 text={_(msg`See this guide`)}
               />{' '}
               for more information.
