ebcaf2f722c8c3c077b3d196bd3ab70fba067a76
Rudy Fraser <him@rudyfraser.com>
2025-10-18 09:56:17 -0400

    WIP: appview changes

--- bskyogcard/src/config.ts
+++ bskyogcard/src/config.ts
@@ -31,7 +31,7 @@ export const envToCfg = (env: Environment): Config => {
   const serviceCfg: ServiceConfig = {
     port: env.port ?? 3000,
     version: env.version,
-    appviewUrl: env.appviewUrl ?? 'https://api.bsky.app',
+    appviewUrl: env.appviewUrl ?? 'https://api.blacksky.community',
     originVerify: env.originVerify,
   }
   return {
