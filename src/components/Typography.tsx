8aa0a8fa52f8cc490d829ad491a2be53cbf720aa
Rishi Balakrishnan <rishi.balakrishnan@gmail.com>
2026-06-23 14:30:42 -0400

    fix: stabilize native rich text rendering

--- src/components/Typography.tsx
+++ src/components/Typography.tsx
@@ -66,7 +66,9 @@ export function Text({
   if (selectable && ios(true)) {
     return (
       <UITextView {...shared}>
-        {renderChildrenWithEmoji(children, shared, emoji ?? false)}
+        {renderChildrenWithEmoji(children, shared, emoji ?? false, {
+          allowNestedUITextView: false,
+        })}
       </UITextView>
     )
   }
