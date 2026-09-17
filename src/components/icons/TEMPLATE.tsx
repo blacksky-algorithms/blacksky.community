4a923ce0a7022f967ab1b46f4a18af698eb7d52f
Rishi Balakrishnan <rishi.balakrishnan@gmail.com>
2026-09-17 12:41:06 -0400

    Revert "style(verification): color verification badges"
    This reverts commit c6966b3c0556397308d4b1cd1fe786d923367bb9.

--- src/components/icons/TEMPLATE.tsx
+++ src/components/icons/TEMPLATE.tsx
@@ -52,23 +52,18 @@ export const IconTemplate_Stroke2_Corner0_Rounded = forwardRef(
 export function createSinglePathSVG({
   path,
   viewBox,
-  defaultFill,
   strokeWidth = 0,
   strokeLinecap = 'butt',
   strokeLinejoin = 'miter',
 }: {
   path: string
   viewBox?: string
-  defaultFill?: Props['fill']
   strokeWidth?: number
   strokeLinecap?: 'butt' | 'round' | 'square'
   strokeLinejoin?: 'miter' | 'round' | 'bevel'
 }) {
   const Icon = forwardRef<Svg, Props>(function LogoImpl(props, ref) {
-    const {fill, size, style, gradient, ...rest} = useCommonSVGProps({
-      ...props,
-      fill: props.fill ?? defaultFill,
-    })
+    const {fill, size, style, gradient, ...rest} = useCommonSVGProps(props)
 
     const hasStroke = strokeWidth > 0
 
